4ea8ae934f3fdc11c662267072c663488fee5497
Yonggao Pan <pan.yonggao@qq.com>
2015-03-03 00:11:19 +0800

    Delete publish.sh

--- publish.sh
+++
@@ -1,7 +0,0 @@
-demeteorizer -o ../build
-cp -Rf ../build/ ../../36h/auto2o/
-cd ../../36h/auto2o
-svn revert programs/server/boot.js
-svn st | awk '{if ( $1 == "?") { print $2}}' | xargs svn add
-svn commit -m 'update'
-bae app publish
