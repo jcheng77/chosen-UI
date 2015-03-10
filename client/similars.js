1f38179da64a4a9f5b570105d1df2b75b4942c04
Yonggao Pan <pan.yonggao@qq.com>
2015-03-10 07:59:20 +0000

    update wp link and using hdpic for similar cars

--- client/similars.js
+++ client/similars.js
@@ -19,6 +19,14 @@ Template.similars.helpers({
     } else {
       return [];
     }
+  },
+  thumbImageUrl: function() {
+    var car = this;
+    if (car.hd_pics && car.hd_pics.length) {
+      return car.hd_pics[0];
+    } else {
+      return "";
+    }
   }
 });
 
--- publish_prod.sh
+++ publish_prod.sh
@@ -2,10 +2,10 @@ demeteorizer -o ../build
 cp -Rf ../build/* ../auto2o/
 cd ../auto2o
 
+svn update
 svn revert programs/server/boot.js
 
 svn st | awk '{if ( $1 == "?") { print $2}}' | xargs svn add
-svn st | grep '^!' | awk '{print $2}' | xargs svn delete --force
 
 svn commit -m 'update'
 bae app publish
